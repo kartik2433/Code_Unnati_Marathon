6caf98d0f29e71292dbec08ade0cd2ec86c45cbe
Patel Kaushal Narayanbhai <patelkaushal2807@gmail.com>
2024-02-27 19:55:03 +0530

    Final Front End Page

--- script.js
+++ script.js
@@ -1,14 +1,55 @@
-function showInput(e) {
+const nameInput = document.getElementById("name");
+const ageInput = document.getElementById("age");
+const selectedGenderInput = document.getElementById("gender")
+const chestInput = document.getElementById("chest");
+const restingInput = document.getElementById("resting");
+const cholersterolInput = document.getElementById("serum");
+const bloodsugarInput = document.getElementById("blood sugar");
+const electrocardiographicInput = document.getElementById("electrocardiographic");
+const heartrateInput = document.getElementById("heart rate");
+const anginaInput = document.getElementById("angina");
+const STInput = document.getElementById("ST");
+const STsegmentInput = document.getElementById("ST segment");
+const STflouroscopyInput = document.getElementById("ST flouroscopy");
+const thalInput = document.getElementById("thal");
+
+nameInput.addEventListener('input', showInput)
+ageInput.addEventListener('input', showInput)
+selectedGenderInput.addEventListener('input', showInput)
+chestInput.addEventListener('input', showInput)
+restingInput.addEventListener('input', showInput)
+cholersterolInput.addEventListener('input', showInput)
+bloodsugarInput.addEventListener('input', showInput)
+electrocardiographicInput.addEventListener('input', showInput)
+heartrateInput.addEventListener('input', showInput)
+anginaInput.addEventListener('input', showInput)
+STInput.addEventListener('input', showInput)
+STsegmentInput.addEventListener('input', showInput)
+STflouroscopyInput.addEventListener('input', showInput)
+thalInput.addEventListener('input', showInput)
+
+function showInput() {
 
 
-    const nameValue = document.getElementById("name").value;
-    const ageValue = document.getElementById("age").value;
-    const selectedGender = document.getElementById("gender").value
-    const localityValue = document.getElementById("locality").value;
-    const heightValue = document.getElementById("height").value;
-    const weightValue = document.getElementById("weight").value;
+  const nameValue = nameInput.value;
+  const ageValue = ageInput.value;
+  const selectedGender = selectedGenderInput.value
+  const chestValue = chestInput.value;
+  const restingValue = restingInput.value;
+  const cholersterolValue = cholersterolInput.value;
+  const bloodsugarValue = bloodsugarInput.value;
+  const electrocardiographicValue = electrocardiographicInput.value;
+  const heartrateValue = heartrateInput.value;
+  const anginaValue = anginaInput.value;
+  const STValue = STInput.value;
+  const STsegmentValue = STsegmentInput.value;
+  const STflouroscopyValue = STflouroscopyInput.value;
+  const thal = thalInput.value;
 
-  const final = document.getElementById("display").innerHTML = `Name : ${nameValue} <br> Age : ${ageValue} <br> Gender : ${selectedGender} <br> Locality : ${localityValue} <br> Height : ${heightValue} <br> Weight : ${weightValue} <br> `;
 
-    e.preventDefault(final);
+
+  document.getElementById("display").innerHTML = `Name : ${nameValue} <br> Age : ${ageValue} <br> Gender : ${selectedGender} <br> Chest Value : ${chestValue} <br> Resting Blood Pressure : ${restingValue} <br> Serum Cholestrol Value : ${cholersterolValue} <br> Fasting blood sugar : ${bloodsugarValue} <br> Resting Electrocardiographic : ${electrocardiographicValue} <br> Maximum Heart Rate : ${heartrateValue} <br> Angina Exercise : ${anginaValue} <br> ST depression : ${STValue} <br> ST Segment : ${STsegmentValue} <br>  Flouroscopy Value : ${STflouroscopyValue} <br> Thal Value : ${thal} <br>`;
+
+
 }
+
