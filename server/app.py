0f0efa7d63c2f5c3058994abd619c7838ca0e5db
Kartik Bhatiya <95977507+kartik2433@users.noreply.github.com>
2024-02-28 18:05:50 +0000

    Added Model Using

--- server/app.py
+++ server/app.py
@@ -5,6 +5,30 @@ import pandas as pd
 import numpy as np
 from request import RequestModel
 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+# Load the dataset (replace 'heart.csv' with your dataset)
+data = pd.read_csv('./model/heart.xls')
+
+# Preprocessing the data
+X = data.drop('target', axis=1)
+y = data['target']
+
+# Splitting the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Feature scaling
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+# Training the model
+model = RandomForestClassifier(random_state=42)
+model.fit(X_train, y_train)
+
 app = FastAPI()
 pickle_in = open("./model/heartModel.pkl", "rb")
 classifier = pickle.load(pickle_in)
@@ -44,7 +68,9 @@ def predict(req : RequestModel):
         req.major_vessels,
         req.thalassemia
     ])
-    prediction = classifier.predict([values])
+    print(type(values))
+    user_data = scaler.transform([values])
+    prediction = model.predict([values])
     print(prediction)
     return {"Prediction": int(prediction[0])}
 
